ac41afb56abd89de2f6225d774f6e5cd6173239e
Richard Hull <rm_hull@yahoo.co.uk>
2016-07-29 00:00:37 +0100

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject rm-hull/lein-nvd-scan "0.1.0"
+  :description "National Vulnerability Database [https://nvd.nist.gov/] dependency-checker plugin for Leiningen."
+  :url "https://github.com/rm-hull/lein-nvd-scan"
+  :license {
+    :name "The MIT License (MIT)"
+    :url "http://opensource.org/licenses/MIT"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]]
+  :scm {:url "git@github.com:rm-hull/lein-nvd-scan.git"}
+  :source-paths ["src"]
+  :jar-exclusions [#"(?:^|/).git"]
+  :codox {
+    :source-paths ["src"]
+    :output-path "doc/api"
+    :source-uri "http://github.com/rm-hull/lein-nvd-scan/blob/master/{filepath}#L{line}"  }
+  :min-lein-version "2.6.1"
+  :profiles {
+    :dev {
+      :global-vars {*warn-on-reflection* true}
+      :plugins [
+        [lein-codox "0.9.5"]
+        [lein-cloverage "1.0.6"]]}})
