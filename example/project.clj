107f223db0b5654f6d7986bf87cdd9a4b7bb8e0c
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-22 00:57:50 +0100

    Update example deps

--- example/project.clj
+++ example/project.clj
@@ -4,11 +4,21 @@
     :name "The MIT License (MIT)"
     :url "http://opensource.org/licenses/MIT"}
   :dependencies [
+    ; No known vulnerabilities, but have dependencies
+    [org.clojure/data.json "0.2.6"]
+    [korma "0.4.3"]
+
+    ; Sub-dependency has MEDIUM rated-vulnerabilities
+    [org.apache.maven.wagon/wagon-http "2.2"]
+
+    ; Has HIGH severity vulnerabilities
     [com.fasterxml.jackson.core/jackson-databind "2.4.2"]
     [com.fasterxml.jackson.core/jackson-annotations "2.4.0"]]
   :source-paths ["src"]
   :min-lein-version "2.6.1"
   :profiles {
     :dev {
+      :dependencies [
+        [org.clojure/clojure "1.8.0"]]
       :plugins [
         [lein-nvd "0.2.0"]]}})
