fb0e9e87499c8e9e647d41e1ab0d7e186d22e140
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-05 16:09:36 +0000

    Update deps

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
     [clansi "1.0.0"]
     [org.clojure/data.json "0.2.6"]
     [org.slf4j/slf4j-simple "1.7.21"]
-    [org.owasp/dependency-check-core "1.4.3"]
+    [org.owasp/dependency-check-core "1.4.4"]
     [rm-hull/table "0.6.4"]]
   :scm {:url "git@github.com:rm-hull/lein-nvd.git"}
   :source-paths ["src"]
--- test/nvd/config_test.clj
+++ test/nvd/config_test.clj
@@ -34,7 +34,7 @@
 
 (deftest check-with-config
   (with-config [project "test/resources/opts.json"]
-    (is (true?  (.endsWith (.getAbsolutePath (io/file (get-in project [:nvd :data-directory]))) "/.m2/repository/org/owasp/dependency-check-utils/1.4.3/data")))
+    (is (true?  (.endsWith (.getAbsolutePath (io/file (get-in project [:nvd :data-directory]))) "/.m2/repository/org/owasp/dependency-check-utils/1.4.4/data")))
     (is (= (get-in project [:nvd :suppression-file]) "suppress.xml"))
     (is (false? (get-in project [:nvd :analyzer :assembly-enabled])))
     (is (true? (get-in project [:nvd :analyzer :cmake-enabled])))
