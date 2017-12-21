4bbf0cea8cd03cdb43ec95b337384e694ae42a59
Richard Hull <rm_hull@yahoo.co.uk>
2017-12-21 09:02:10 +0000

    Use clojure 1.9.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
     :name "The MIT License (MIT)"
     :url "http://opensource.org/licenses/MIT"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"]
+    [org.clojure/clojure "1.9.0"]
     [clansi "1.0.0"]
     [org.clojure/data.json "0.2.6"]
     [org.slf4j/slf4j-simple "1.7.25"]
