d01e484af4e91d3a9a50ace8badcb8398acb1b87
Richard Hull <rm_hull@yahoo.co.uk>
2018-01-16 21:11:22 -0500

    Update to latest dependency-check jar

--- test/nvd/config_test.clj
+++ test/nvd/config_test.clj
@@ -26,7 +26,7 @@
    [clojure.test :refer :all]
    [nvd.config :refer :all]))
 
-(def dependency-check-version "3.0.2")
+(def dependency-check-version "3.1.0")
 
 (deftest check-app-name
   (is (= "unknown" (app-name {:nome "hello-world" :version "0.0.1"})))
