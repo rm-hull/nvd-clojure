bc6a9557d46a8c2ce71f2b70bb2240f38d6e4131
Richard Hull <rm_hull@yahoo.co.uk>
2018-11-12 11:45:49 +0000

    Version 0.5.6

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-nvd "0.5.5"
+(defproject lein-nvd "0.5.6"
   :description "National Vulnerability Database [https://nvd.nist.gov/] dependency-checker leiningen plugin."
   :url "https://github.com/rm-hull/lein-nvd"
   :license {
