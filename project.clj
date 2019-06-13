b65ab7756a6e219bea4e49ec2d562d5f92f49ed4
Richard Hull <rm_hull@yahoo.co.uk>
2019-06-13 09:46:59 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
       :global-vars {*warn-on-reflection* true}
       :plugins [
         [lein-cljfmt "0.6.4"]
-        [lein-codox "0.10.6"]
+        [lein-codox "0.10.7"]
         [lein-cloverage "1.1.1"]]
       :dependencies [
         [commons-collections "3.2.1"]]}})
