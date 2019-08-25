1f98682ad25a88d5761e613792730300c871247d
Richard Hull <rm_hull@yahoo.co.uk>
2019-08-25 22:57:02 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
     [org.clojure/clojure "1.10.1"]
     [clansi "1.0.0"]
     [org.clojure/data.json "0.2.6"]
-    [org.slf4j/slf4j-simple "1.7.26"]
-    [org.owasp/dependency-check-core "5.2.0"]
-    [rm-hull/table "0.7.0"]
+    [org.slf4j/slf4j-simple "1.7.28"]
+    [org.owasp/dependency-check-core "5.2.1"]
+    [rm-hull/table "0.7.1"]
     [trptcolin/versioneer "0.2.0"]]
   :scm {:url "git@github.com:rm-hull/lein-nvd.git"}
   :source-paths ["src"]
