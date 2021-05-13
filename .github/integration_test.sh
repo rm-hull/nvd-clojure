b6064b961b344033761c6464afb9dcad5e0f8d85
vemv <vemv@users.noreply.github.com>
2021-05-13 14:47:39 +0200

    CLI: support passing a config-filename and a classpath
    By supporting both at the same time, one can have version-controlled config files (as opposed to dynamically generated temp files having an unstable `classpath` entry).

--- .github/integration_test.sh
+++ .github/integration_test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -Euxo pipefail
 
+CONFIG_FILE="$PWD/.github/nvd-config.json"
+SUCCESS_REGEX="[1-9][0-9] vulnerabilities detected\. Severity: "
+
 original=$(pwd)
 
 lein install
@@ -16,7 +19,7 @@ if lein nvd check > example-lein-output; then
   exit 1
 fi
 
-if ! grep --silent "[1-9][0-9] vulnerabilities detected\. Severity: " example-lein-output; then
+if ! grep --silent "$SUCCESS_REGEX" example-lein-output; then
   echo "Should have found vulnerabilities!"
   exit 1
 fi
@@ -28,12 +31,12 @@ example_classpath="$(lein with-profile -user,-dev,-test classpath)"
 # cd to the root dir, so that one runs `defproject nvd-clojure` which is the most clean and realistic way to run `main`:
 cd .. || exit 1
 
-if lein with-profile -user,-dev run -m nvd.task.check "" "$example_classpath" > example-lein-output; then
+if lein with-profile -user,-dev run -m nvd.task.check "$CONFIG_FILE" "$example_classpath" > example-lein-output; then
   echo "Should have failed with non-zero code!"
   exit 1
 fi
 
-if ! grep --silent "[1-9][0-9] vulnerabilities detected\. Severity: " example-lein-output; then
+if ! grep --silent "$SUCCESS_REGEX" example-lein-output; then
   echo "Should have found vulnerabilities!"
   exit 1
 fi
@@ -47,12 +50,12 @@ example_classpath="$(clojure -Spath)"
 # cd to the root dir, so that one runs `defproject nvd-clojure` which is the most clean and realistic way to run `main`:
 cd .. || exit 1
 
-if clojure -m nvd.task.check "" "$example_classpath" > example-lein-output; then
+if clojure -m nvd.task.check "$CONFIG_FILE" "$example_classpath" > example-lein-output; then
   echo "Should have failed with non-zero code!"
   exit 1
 fi
 
-if ! grep --silent "[1-9][0-9] vulnerabilities detected\. Severity: " example-lein-output; then
+if ! grep --silent "$SUCCESS_REGEX" example-lein-output; then
   echo "Should have found vulnerabilities!"
   exit 1
 fi
