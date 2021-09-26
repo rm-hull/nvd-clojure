b1471e1c929d74f6df1917c00d436375e81e0292
rm-hull <rm-hull@users.noreply.github.com>
2021-09-26 07:37:53 +0000

    Updating stale dependencies

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [rm-hull/table "0.7.1"]
                  [trptcolin/versioneer "0.2.0"]
                  [org.clojure/java.classpath "1.0.0"]
-                 [org.clojure/tools.deps.alpha "0.12.1036" :exclusions [org.slf4j/jcl-over-slf4j]]
+                 [org.clojure/tools.deps.alpha "0.12.1048" :exclusions [org.slf4j/jcl-over-slf4j]]
                  ;; Explicitly depend on a certain Jackson, consistently.
                  ;; Otherwise, when using the Lein plugin, Leiningen's own dependencies can pull a different Jackson version
                  ;; (see https://github.com/jeremylong/DependencyCheck/issues/3441):
@@ -19,13 +19,13 @@
                  [com.fasterxml.jackson.core/jackson-annotations "2.12.5"]
                  [com.fasterxml.jackson.core/jackson-core "2.12.5"]
                  [com.fasterxml.jackson.module/jackson-module-afterburner "2.12.5"]
-                 [org.apache.maven.resolver/maven-resolver-transport-http "1.7.1" #_"Fixes a CVE"]
+                 [org.apache.maven.resolver/maven-resolver-transport-http "1.7.2" #_"Fixes a CVE"]
                  [org.apache.maven/maven-core "3.8.2" #_"Fixes a CVE"]
                  [org.eclipse.jetty/jetty-client "11.0.6" #_"Fixes a CVE"]
-                 [org.apache.maven.resolver/maven-resolver-spi "1.7.1" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-api "1.7.1" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-util "1.7.1" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-impl "1.7.1" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-spi "1.7.2" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-api "1.7.2" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-util "1.7.2" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-impl "1.7.2" #_"Satisfies :pedantic?"]
                  [org.apache.maven/maven-resolver-provider "3.8.2" #_"Satisfies :pedantic?"]]
   :scm {:url "git@github.com:rm-hull/nvd-clojure.git"}
   :source-paths ["src"]
@@ -42,7 +42,7 @@
                              [lein-cloverage "1.2.2"]
                              [lein-ancient "0.7.0"]
                              [jonase/eastwood "0.9.9"]]
-                   :dependencies [[clj-kondo "2021.08.06"]
+                   :dependencies [[clj-kondo "2021.09.25"]
                                   [commons-collections "20040616"]]}
              :ci {:pedantic? :abort}
-             :clj-kondo {:dependencies [[clj-kondo "2021.08.06"]]}})
+             :clj-kondo {:dependencies [[clj-kondo "2021.09.25"]]}})
