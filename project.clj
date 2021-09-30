b8cf5d3d98e7357432e0fb69fe6aacf6fe488ef2
vemv <vemv@users.noreply.github.com>
2021-09-30 10:10:30 +0200

    1.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nvd-clojure "1.6.0"
+(defproject nvd-clojure "1.7.0"
   :description "National Vulnerability Database [https://nvd.nist.gov/] dependency-checker"
   :url "https://github.com/rm-hull/nvd-clojure"
   :license {:name "The MIT License (MIT)"
