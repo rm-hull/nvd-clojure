09a80b773a89217fe635341348c285d2551a42a7
Sean Corfield <sean@corfield.org>
2021-11-19 06:30:47 -0800

    clean up patch for setting up tools manually

--- .github/integration_test.sh
+++ .github/integration_test.sh
@@ -11,16 +11,8 @@ if ! lein with-profile -user,-dev,+ci install; then
   exit 1
 fi
 
-# more debugging:
-date
-ls -lR /opt/hostedtoolcache/ClojureToolsDeps/1.10.3-1029/x64/lib/clojure
-ls -lR /home/runner/.config/clojure
-
-# prep the CLI -- this should not be needed:
-clojure -P
-/home/runner/.config/clojure
 # manually setup tools -- this should be auto-created:
-mkdir /home/runner/.config/clojure/tools
+mkdir -p /home/runner/.config/clojure/tools
 echo '{:lib io.github.clojure/tools.tools :coord {:git/tag "v0.2.2" :git/sha "e1febed7ddaa5be15721255c13eb68e11bbbb398"}}' > /home/runner/.config/clojure/tools/tools.edn
 
 if ! clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "RELEASE"}' :as nvd; then
