ccbc3350612bad5df3c723a95af818b00c3a3b27
Sean Corfield <sean@corfield.org>
2021-11-18 21:07:31 -0800

    Initial CLI prep

--- .github/integration_test.sh
+++ .github/integration_test.sh
@@ -11,6 +11,8 @@ if ! lein with-profile -user,-dev,+ci install; then
   exit 1
 fi
 
+# prep the CLI -- this should not be needed:
+clojure -P
 # manually setup tools -- this should be auto-created:
 mkdir /home/runner/.config/clojure/tools
 echo '{:lib io.github.clojure/tools.tools :coord {:git/tag "v0.2.2" :git/sha "e1febed7ddaa5be15721255c13eb68e11bbbb398"}}' > /home/runner/.config/clojure/tools/tools.edn
