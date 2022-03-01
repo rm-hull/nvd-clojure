b9e29ea6408bc8f099a617df7ffdcebbca4d0641
vemv <vemv@users.noreply.github.com>
2022-03-01 07:38:44 +0000

    Update stale dependencies

--- project.clj
+++ project.clj
@@ -7,24 +7,24 @@
                  [clansi "1.0.0"]
                  [org.clojure/data.json "2.4.0"]
                  [org.slf4j/slf4j-simple "2.0.0-alpha1"]
-                 [org.owasp/dependency-check-core "6.5.3"]
+                 [org.owasp/dependency-check-core "7.0.1"]
                  [rm-hull/table "0.7.1"]
                  [trptcolin/versioneer "0.2.0"]
                  ;; Explicitly depend on a certain Jackson, consistently.
                  ;; Otherwise, when using the Lein plugin, Leiningen's own dependencies can pull a different Jackson version
                  ;; (see https://github.com/jeremylong/DependencyCheck/issues/3441):
-                 [com.fasterxml.jackson.core/jackson-databind "2.13.1"]
-                 [com.fasterxml.jackson.core/jackson-annotations "2.13.1"]
-                 [com.fasterxml.jackson.core/jackson-core "2.13.1"]
-                 [com.fasterxml.jackson.module/jackson-module-afterburner "2.13.1"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.13.2"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.13.2"]
+                 [com.fasterxml.jackson.core/jackson-core "2.13.2"]
+                 [com.fasterxml.jackson.module/jackson-module-afterburner "2.13.2"]
                  [org.apache.maven.resolver/maven-resolver-transport-http "1.7.3" #_"Fixes a CVE"]
-                 [org.apache.maven/maven-core "3.8.4" #_"Fixes a CVE"]
-                 [org.eclipse.jetty/jetty-client "11.0.7" #_"Fixes a CVE" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.maven/maven-core "3.8.5" #_"Fixes a CVE"]
+                 [org.eclipse.jetty/jetty-client "11.0.8" #_"Fixes a CVE" :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.maven.resolver/maven-resolver-spi "1.7.3" #_"Satisfies :pedantic?"]
                  [org.apache.maven.resolver/maven-resolver-api "1.7.3" #_"Satisfies :pedantic?"]
                  [org.apache.maven.resolver/maven-resolver-util "1.7.3" #_"Satisfies :pedantic?"]
                  [org.apache.maven.resolver/maven-resolver-impl "1.7.3" #_"Satisfies :pedantic?"]
-                 [org.apache.maven/maven-resolver-provider "3.8.4" #_"Satisfies :pedantic?"]
+                 [org.apache.maven/maven-resolver-provider "3.8.5" #_"Satisfies :pedantic?"]
                  [org.codehaus.plexus/plexus-utils "3.4.1" #_"Satisfies :pedantic?"]]
   :scm {:url "git@github.com:rm-hull/nvd-clojure.git"}
   :source-paths ["src"]
@@ -37,15 +37,15 @@
   :target-path "target/%s"
   :profiles {:dev {:plugins [[lein-cljfmt "0.7.0"]
                              [lein-codox "0.10.7"]
-                             [lein-cloverage "1.2.2"]
+                             [lein-cloverage "1.2.3"]
                              [lein-ancient "0.7.0"]
-                             [jonase/eastwood "1.0.0"]]
+                             [jonase/eastwood "1.2.3"]]
                    :eastwood {:add-linters [:boxed-math
                                             :performance]}
-                   :dependencies [[clj-kondo "2022.01.13"]
+                   :dependencies [[clj-kondo "2022.03.09"]
                                   [commons-collections "20040616"]]}
              :ci {:pedantic? :abort}
-             :clj-kondo {:dependencies [[clj-kondo "2022.01.13"]]}
+             :clj-kondo {:dependencies [[clj-kondo "2022.03.09"]]}
              :skip-self-check {:jvm-opts ["-Dnvd-clojure.internal.skip-self-check=true"
                                           "-Dclojure.main.report=stderr"]}}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
