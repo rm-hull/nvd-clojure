17b2336c49efb97c3a2ab788c2246ee9ada6df54
vemv <vemv@users.noreply.github.com>
2023-08-20 07:38:21 +0000

    Upgrade various dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/rm-hull/nvd-clojure"
   :license {:name "The MIT License (MIT)"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [clansi "1.0.0"]
                  [org.clojure/data.json "2.4.0"]
                  [org.slf4j/slf4j-simple "2.0.7"]
-                 [org.owasp/dependency-check-core "8.3.1"]
+                 [org.owasp/dependency-check-core "8.4.0"]
                  [rm-hull/table "0.7.1"]
                  [trptcolin/versioneer "0.2.0"]
                  ;; Explicitly depend on a certain Jackson, consistently.
@@ -16,22 +16,21 @@
                  [com.fasterxml.jackson.core/jackson-annotations "2.15.2"]
                  [com.fasterxml.jackson.core/jackson-core "2.15.2"]
                  [com.fasterxml.jackson.module/jackson-module-afterburner "2.15.2"]
-                 [org.apache.maven.resolver/maven-resolver-transport-http "1.9.10" #_"Fixes a CVE"]
-                 [org.yaml/snakeyaml "2.0" #_"Fixes a CVE"]
-                 [org.apache.maven/maven-core "3.9.2" #_"Fixes a CVE"]
-                 [org.eclipse.jetty/jetty-client "12.0.0.beta1" #_"Fixes a CVE" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.maven.resolver/maven-resolver-spi "1.9.10" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-api "1.9.10" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-util "1.9.10" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-impl "1.9.10" #_"Satisfies :pedantic?"]
-                 [org.apache.maven/maven-resolver-provider "3.9.2" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-transport-http "1.9.15" #_"Fixes a CVE"]
+                 [org.yaml/snakeyaml "2.1" #_"Fixes a CVE"]
+                 [org.apache.maven/maven-core "3.9.4" #_"Fixes a CVE"]
+                 [org.eclipse.jetty/jetty-client "12.0.0" #_"Fixes a CVE" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.maven.resolver/maven-resolver-spi "1.9.15" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-api "1.9.15" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-util "1.9.15" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-impl "1.9.15" #_"Satisfies :pedantic?"]
+                 [org.apache.maven/maven-resolver-provider "3.9.4" #_"Satisfies :pedantic?"]
                  [org.codehaus.plexus/plexus-utils "4.0.0" #_"Satisfies :pedantic?"]]
   :managed-dependencies [[com.google.code.gson/gson "2.10.1"]]
   :scm {:url "git@github.com:rm-hull/nvd-clojure.git"}
   :source-paths ["src"]
   :jar-exclusions [#"(?:^|/).git"]
-  :codox {
-          :source-paths ["src"]
+  :codox {:source-paths ["src"]
           :output-path "doc/api"
           :source-uri "https://github.com/rm-hull/nvd-clojure/blob/master/{filepath}#L{line}"}
   :min-lein-version "2.8.1"
@@ -41,13 +40,13 @@
                              [lein-codox "0.10.7"]
                              [lein-cloverage "1.2.3"]
                              [lein-ancient "0.7.0"]
-                             [jonase/eastwood "1.3.0"]]
+                             [jonase/eastwood "1.4.0"]]
                    :eastwood {:add-linters [:boxed-math
                                             :performance]}
-                   :dependencies [[clj-kondo "2023.05.26"]
+                   :dependencies [[clj-kondo "2023.07.13"]
                                   [commons-collections "20040616"]]}
              :ci {:pedantic? :abort}
-             :clj-kondo {:dependencies [[clj-kondo "2023.05.26"]]}
+             :clj-kondo {:dependencies [[clj-kondo "2023.07.13"]]}
              :skip-self-check {:jvm-opts ["-Dnvd-clojure.internal.skip-self-check=true"]}}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
