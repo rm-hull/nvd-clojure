75fd89b089946d9596832c82a72f22fa0d8e4a47
vemv <vemv@users.noreply.github.com>
2023-10-23 07:37:45 +0000

    Upgrade various dependencies

--- project.clj
+++ project.clj
@@ -7,24 +7,24 @@
                  [clansi "1.0.0"]
                  [org.clojure/data.json "2.4.0"]
                  [org.slf4j/slf4j-simple "2.0.9"]
-                 [org.owasp/dependency-check-core "8.4.0"]
+                 [org.owasp/dependency-check-core "8.4.2"]
                  [rm-hull/table "0.7.1"]
                  [trptcolin/versioneer "0.2.0"]
                  ;; Explicitly depend on a certain Jackson, consistently.
                  ;; (See also: https://github.com/jeremylong/DependencyCheck/issues/3441)
-                 [com.fasterxml.jackson.core/jackson-databind "2.15.2"]
-                 [com.fasterxml.jackson.core/jackson-annotations "2.15.2"]
-                 [com.fasterxml.jackson.core/jackson-core "2.15.2"]
-                 [com.fasterxml.jackson.module/jackson-module-afterburner "2.15.2"]
-                 [org.apache.maven.resolver/maven-resolver-transport-http "1.9.15" #_"Fixes a CVE"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.15.3"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.15.3"]
+                 [com.fasterxml.jackson.core/jackson-core "2.15.3"]
+                 [com.fasterxml.jackson.module/jackson-module-afterburner "2.15.3"]
+                 [org.apache.maven.resolver/maven-resolver-transport-http "1.9.16" #_"Fixes a CVE"]
                  [org.yaml/snakeyaml "2.2" #_"Fixes a CVE"]
-                 [org.apache.maven/maven-core "3.9.4" #_"Fixes a CVE"]
-                 [org.eclipse.jetty/jetty-client "12.0.1" #_"Fixes a CVE" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.maven.resolver/maven-resolver-spi "1.9.15" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-api "1.9.15" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-util "1.9.15" #_"Satisfies :pedantic?"]
-                 [org.apache.maven.resolver/maven-resolver-impl "1.9.15" #_"Satisfies :pedantic?"]
-                 [org.apache.maven/maven-resolver-provider "3.9.4" #_"Satisfies :pedantic?"]
+                 [org.apache.maven/maven-core "3.9.5" #_"Fixes a CVE"]
+                 [org.eclipse.jetty/jetty-client "12.0.2" #_"Fixes a CVE" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.maven.resolver/maven-resolver-spi "1.9.16" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-api "1.9.16" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-util "1.9.16" #_"Satisfies :pedantic?"]
+                 [org.apache.maven.resolver/maven-resolver-impl "1.9.16" #_"Satisfies :pedantic?"]
+                 [org.apache.maven/maven-resolver-provider "3.9.5" #_"Satisfies :pedantic?"]
                  [org.codehaus.plexus/plexus-utils "4.0.0" #_"Satisfies :pedantic?"]]
   :managed-dependencies [[com.google.code.gson/gson "2.10.1"]]
   :scm {:url "git@github.com:rm-hull/nvd-clojure.git"}
@@ -43,10 +43,10 @@
                              [jonase/eastwood "1.4.0"]]
                    :eastwood {:add-linters [:boxed-math
                                             :performance]}
-                   :dependencies [[clj-kondo "2023.09.07"]
+                   :dependencies [[clj-kondo "2023.10.20"]
                                   [commons-collections "20040616"]]}
              :ci {:pedantic? :abort}
-             :clj-kondo {:dependencies [[clj-kondo "2023.09.07"]]}
+             :clj-kondo {:dependencies [[clj-kondo "2023.10.20"]]}
              :skip-self-check {:jvm-opts ["-Dnvd-clojure.internal.skip-self-check=true"]}}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
