2599586c8c75e017e03eb4cf07e180ad99649974
rm-hull <rm-hull@users.noreply.github.com>
2025-09-26 07:39:20 +0000

    Update stale dependencies

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
                  [clansi "1.0.0"]
                  ;; dependency-check-core transitively brings in two versions of
                  ;; this dependency, so we explicitly depend on the latest
-                 [com.google.errorprone/error_prone_annotations "2.38.0"]
+                 [com.google.errorprone/error_prone_annotations "2.42.0"]
                  [org.clojure/data.json "2.5.1"]
                  [org.slf4j/slf4j-simple "2.0.17"]
-                 [org.owasp/dependency-check-core "12.1.3"]
+                 [org.owasp/dependency-check-core "12.1.6"]
                  [rm-hull/table "0.7.1"]
                  [trptcolin/versioneer "0.2.0"]]
-  :managed-dependencies [[com.google.code.gson/gson "2.13.1"]]
+  :managed-dependencies [[com.google.code.gson/gson "2.13.2"]]
   :scm {:url "git@github.com:rm-hull/nvd-clojure.git"}
   :source-paths ["src"]
   :jar-exclusions [#"(?:^|/).git"]
@@ -30,10 +30,10 @@
                              [jonase/eastwood "1.4.0"]]
                    :eastwood {:add-linters [:boxed-math
                                             :performance]}
-                   :dependencies [[clj-kondo "2025.06.05"]
+                   :dependencies [[clj-kondo "2025.09.22"]
                                   [commons-collections "20040616"]]}
              :ci {:pedantic? :abort}
-             :clj-kondo {:dependencies [[clj-kondo "2025.06.05"]]}
+             :clj-kondo {:dependencies [[clj-kondo "2025.09.22"]]}
              :skip-self-check {:jvm-opts ["-Dnvd-clojure.internal.skip-self-check=true"]}}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
